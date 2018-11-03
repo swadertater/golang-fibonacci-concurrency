e96dce5ec88685216d54fadadf200da8c39d984e
Swade Howard <swade.howard@gmail.com>
2018-11-02 19:20:47 -0500

    channels and sync need to be added

--- cs340ExtraCreditProject.go
+++ cs340ExtraCreditProject.go
@@ -5,20 +5,37 @@ import (
 	"time"
 )
 
-type Fibonacci struct {
-	num    int
-	answer int
+func fib(n float64) float64 {
+	if n < 2 {
+		return n
+	}
+	return fib(n-1) + fib(n-2)
 }
 
-func (fib Fibonacci) run() {
+type Fibonacci struct {
+	num    float64
+	answer float64
+}
 
+func (f Fibonacci) run() {
+	if f.num <= 1 {
+		f.answer = f.num
+	} else {
+		var fib1 Fibonacci
+		fib1.num = f.num - 1
+		var fib2 Fibonacci
+		fib2.num = f.num - 2
+		go fib(fib2.num)
+		f.answer = fib1.answer + fib2.answer
+	}
 }
 
 func main() {
 	go spinner(100 * time.Millisecond)
 	const n = 45
-	fibN := fib(n)
+	fibN := 12
 	fmt.Printf("\rFibonacci(%d) = %d\n", n, fibN)
+
 }
 
 func spinner(delay time.Duration) {
@@ -28,15 +45,4 @@ func spinner(delay time.Duration) {
 			time.Sleep(delay)
 		}
 	}
-}
-
-func fib(x int) int {
-	if x < 2 {
-		return x
-	}
-	return fib(x-1) + fib(x-2)
-}
-
-func run() {
-
 }
