6a101b297657531869f77d7aef0f27bb1d140737
Swade Howard <swhowar@siue.edu>
2018-11-06 07:39:27 -0600

    Changes made

--- badFibonacci.go
+++ badFibonacci.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"time"
-	//"runtime"
 )
 
 type Fibonacci struct {
--- improvedFibonacci.go
+++ improvedFibonacci.go
@@ -1,44 +1,98 @@
 package main
 
-import(
+import (
 	"fmt"
 	"runtime"
-	"sync"
+	"time"
 )
 
-func main(){
-	numCpus := runtime.NumCPU
-	var wg sync.WaitGroup
-	fmt.Println("We have ", numCpus, " available cores.")
-	jobs := make(chan int, 20)
-	results := make(chan int, 20)
+type Fibonacci struct {
+	num    float64
+	answer float64
+}
 
-	for i := 0; i < 20; i++ {
-		jobs <- i
+type goRoutineManager struct {
+	goRoutineCnt chan bool
+}
+
+func (g *goRoutineManager) Run(f func()) {
+	select {
+	case g.goRoutineCnt <- true:
+		go func() {
+			f()
+			<-g.goRoutineCnt
+		}()
+	default:
+		f()
 	}
+}
 
-	
+func NewGoRoutineManager(goRoutineLimit int) *goRoutineManager {
+	return &goRoutineManager{
+		goRoutineCnt: make(chan bool, goRoutineLimit),
+	}
+}
 
-	for w:= 1; w < 3; w++{
-		go worker(w, jobs, results)
+func newFibonacci(n float64, gm *goRoutineManager) *Fibonacci {
+
+	f := new(Fibonacci)
+	f.num = n
+	c1 := make(chan float64, 1)
+	c2 := make(chan float64, 1)
+
+	if f.num <= 1 {
+		f.answer = n
+	} else {
+		gm.Run(func() {
+			fib1 := newFibonacci(n-1, gm)
+			c2 <- fib1.answer
+		})
+
+		gm.Run(func() {
+			fib2 := newFibonacci(n-2, gm)
+			c1 <- fib2.answer
+		})
+
+		f.answer = <-c2 + <-c1
+	}
+	close(c1)
+	close(c2)
+
+	return f
+}
+
+func main() {
+
+	numbers := []float64{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40}
+	for _, value := range numbers {
+		start := time.Now()
+		fmt.Println("getting the ", value, " fibonacci number")
+		numCpu := runtime.NumCPU()
+		gm := NewGoRoutineManager(numCpu)
+
+		f := newFibonacci(value, gm)
+		fmt.Println(f.answer)
+
+		end := time.Now()
+		totalTime := end.Sub(start)
+		fmt.Println("Fibonacci number: ", value, " took ", totalTime, "\n")
+	}
+	for _, value := range numbers {
+		start := time.Now()
+		fmt.Println("getting the ", value, " fibonacci number")
+
+		fmt.Println(fib(int(value)))
+
+		end := time.Now()
+		totalTime := end.Sub(start)
+		fmt.Println("Fibonacci number: ", value, " took ", totalTime, "\n")
 	}
 
 }
-/*
-func worker(chan int){
 
-}
-*/
-func fib(n int) int{
-	if n <= 1{
+func fib(n int) int {
+	if n <= 1 {
 		return n
 	}
 	return fib(n-1) + fib(n-2)
-}
-
-func worker(id int, jobs <-chan int, results chan<-int){
-	for j:= range jobs{
-		fibonacciNumber := fib(j)
-		fmt.Println("Woker: ", id, " has completed the job and returned", fibonacciNumber)
-	}
 }
